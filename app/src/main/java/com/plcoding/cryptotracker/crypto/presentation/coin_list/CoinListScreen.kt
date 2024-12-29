e6464c42678833f74f5f23da8299044490defdf4
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-30 01:13:52 +0530

    Build Coin List ViewModel

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import com.plcoding.cryptotracker.crypto.presentation.model.CoinListState
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 
 @Composable
--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -1,6 +1,7 @@
-package com.plcoding.cryptotracker.crypto.presentation.model
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
 
 import androidx.compose.runtime.Immutable
+import com.plcoding.cryptotracker.crypto.presentation.model.CoinUi
 
 @Immutable
 data class CoinListState(
